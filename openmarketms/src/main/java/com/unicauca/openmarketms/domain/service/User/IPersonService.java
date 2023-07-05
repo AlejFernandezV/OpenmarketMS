11301b4a10297b9cc24c97be8e2fe7a455aa1256
Geraldine <geralmore@unicauca.edu.co>
2023-07-05 00:48:09 -0500

    login

--- openmarketms/src/main/java/com/unicauca/openmarketms/domain/service/User/IPersonService.java
+++ openmarketms/src/main/java/com/unicauca/openmarketms/domain/service/User/IPersonService.java
@@ -1,5 +1,18 @@
 package com.unicauca.openmarketms.domain.service.User;
 
 public interface IPersonService {
-    
+    /**
+     * Logueo del usuario
+     * @param email email del usuario
+     * @param password contraseña del usuario
+     * @return Objeto persona si el logueo es exitoso, null de lo contrario
+     */
+    public Person login(String email, String password);
+
+    /**
+     * Creación de un nuevo usuario
+     * @param person Objeto persona a crear
+     * @return Objeto persona creado
+     */
+    public Person create(Person person);
 }
--- openmarketms/src/main/java/com/unicauca/openmarketms/presentation/rest/Person/PersonController.java
+++ openmarketms/src/main/java/com/unicauca/openmarketms/presentation/rest/Person/PersonController.java
@@ -1,5 +1,11 @@
 package com.unicauca.openmarketms.presentation.rest.Person;
 
 public class PersonController {
-    
+    @Autowired
+    private IPersonService personService;
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET, produces = "application/json")
+    public Person login(String email, String password){
+        return personService.login(email, password);
+    }
 }
